f80a097bd483bb5e31de4e40cb62896b7740379b
Tomisin Babalola <ademolababalola2003@gmail.com>
2022-08-09 16:02:18 +0100

    completion

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
 const inputs = document.querySelector(".inputs");
 const resetBtn = document.querySelector(".reset-btn");
 const hint = document.querySelector(".hint span");
+const guessesLeft = document.querySelector(".guess-left span");
 const wrongLetter = document.querySelector(".wrong-letter span");
 const typingInput = document.querySelector(".typing-input");
 
 let word,
+  maxGuesses,
+  corrects = [],
   incorrects = [];
 
 function randomWord() {
   // getting random object from wordList
   let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
   word = ranObj.word; // getting word of random object
-  console.log(word);
+  maxGuesses = 8;
+  (corrects = []), (incorrects = []);
 
   hint.innerText = ranObj.hint;
+  guessesLeft.innerText = maxGuesses;
+  wrongLetter.innerText = incorrects;
 
   let html = "";
   for (let i = 0; i < word.length; i++) {
@@ -25,23 +31,46 @@ randomWord();
 
 function initGame(e) {
   let key = e.target.value;
-  if (key.match(/^[A-Za-z]+$/)) {
-    console.log(key);
+  if (
+    key.match(/^[A-Za-z]+$/) &&
+    !incorrects.includes(` ${key}`) &&
+    !corrects.includes(key)
+  ) {
     if (word.includes(key)) {
       // if user letter found in the word
       for (let i = 0; i < word.length; i++) {
+        // showing matched letters in input
         if (word[i] === key) {
-          // showing matched letters in input
+          corrects.push(key);
           inputs.querySelectorAll("input")[i].value = key;
         }
       }
     } else {
-      incorrects.push(key);
+      maxGuesses--; // decrement maxGuesses by 1
+      incorrects.push(` ${key}`);
     }
+    guessesLeft.innerText = maxGuesses;
+    wrongLetter.innerText = incorrects;
   }
   typingInput.value = "";
+
+  setTimeout(() => {
+    if (corrects.length === word.length) {
+      // if users found all letters
+      alert(`Congrats! You have found the word ${word.toUpperCase()}`);
+      randomWord(); // calling randomfunc to reset game
+    } else if (maxGuesses < 1) {
+      // if users couldn't find all letters
+      alert("Game over! You have no guesses left");
+      for (let i = 0; i < word.length; i++) {
+        // showing matched letters in input
+        inputs.querySelectorAll("input")[i].value = word[i];
+      }
+    }
+  });
 }
 
 resetBtn.addEventListener("click", randomWord);
 typingInput.addEventListener("input", initGame);
+inputs.addEventListener("click", () => typingInput.focus());
 document.addEventListener("keydown", () => typingInput.focus());
